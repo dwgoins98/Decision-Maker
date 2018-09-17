aede0b91c294d3ea67c69903022a4804c1930824
Daniel Goins <dwgoins98@gmail.com>
2018-09-17 12:49:21 -0400

    Initial Commit

---
+++ Francolino HW/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Francolino HW
+//
+//  Created by Daniel Goins on 9/17/18.
+//  Copyright © 2018 Daniel Goins. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
